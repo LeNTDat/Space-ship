e108986fd37847f69927149cbd0616e53ec70717
DESKTOP-3PUVA80\Admin <dat.le@alpaca.vn>
2023-04-07 21:03:01 +0700

    new rocket, collision, thrusting, audio.

--- Space Ship/Assets/Script/Movement.cs
+++ Space Ship/Assets/Script/Movement.cs
@@ -5,26 +5,67 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody rb;
-    int speed = 1;
-    float yPos;
+    float speed = 1000f;
+    float RotateSpeed = 180f;
+    AudioSource thrustingST;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        thrustingST = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveShip();
+        AudioThrust();
     }
 
-    void MoveShip()
+    private void FixedUpdate()
     {
-        if (Input.GetButtonDown("Jump"))
+        ThrustingShip();
+        RotateShip();
+    }
+
+    void ThrustingShip()
+    {
+        if (Input.GetButton("Jump"))
         {
-            yPos += speed; 
-            rb.velocity = new Vector3(0, yPos, 0);
+            rb.AddRelativeForce(Vector3.up  * speed * Time.deltaTime);
         }
+        
+    }
+
+    void AudioThrust()
+    {
+        if (Input.GetButtonDown("Jump")) { 
+            thrustingST.Play();
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            thrustingST.Stop();
+        }
+
+    }
+
+    void RotateShip()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyTheRotation(RotateSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            ApplyTheRotation(-RotateSpeed);
+        }
+
+    }
+
+    void ApplyTheRotation(float Rotate)
+    {
+        Vector3 zInput = Vector3.forward * Time.deltaTime * Rotate;
+        rb.freezeRotation = true;
+        transform.Rotate(zInput);
+        rb.freezeRotation = false;
     }
 }
